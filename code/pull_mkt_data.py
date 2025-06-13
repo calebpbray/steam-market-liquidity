dc3165b04c4ae2683cd05a809d9140022b28c128
Caleb Bray <calebpbray@gmail.com>
2025-06-13 17:30:39 -0500

    Added function to pull DOTA2 prices, added treated_unit and post_treat flags.

--- code/pull_mkt_data.py
+++ code/pull_mkt_data.py
@@ -9,21 +9,27 @@ import pandas as pd
 from steamcrawl import Request
 import re
 import os
-from pull_mkt_data_func import pull_cs_price_history
+from pull_mkt_data_func import pull_cs_price_history, pull_dota_price_history
 
 #%% FILEPATHS
 home_dir = os.path.abspath(os.path.join(os.path.dirname(__file__),".."))
 code_dir = os.getcwd()
 data_dir = os.path.abspath(os.path.join(os.path.dirname(__file__),"..")) + "/data"
+cs_data_dir = data_dir + "/cs"
+dota_data_dir = data_dir + "/dota2"
 #%% SETUP
 #need "URL Decoded" steamLoginSecure from steamcommunity.com (NOT store.steampowered.com !!)
 with open(home_dir + "/steamLoginSecure.txt", 'r') as file:
     steamLoginSecure = file.read().strip()
 request = Request(steamLoginSecure) #doesn't work on work pc endpoint
 
-#read each line (each corresponding to one item) in the file into a list
+#read each line (each corresponding to one CS item) in the file into a list
 with open(home_dir + "/cs_item_list.txt", 'r') as file:
     cs_item_list = [line.strip() for line in file]
+    
+#read each line (each corresponding to one dota2 item) in the file into a list
+with open(home_dir + "/dota_item_list.txt", 'r') as file:
+    dota_item_list = [line.strip() for line in file]
 
 #possible skins, ak47 elite build, m4a1s basilisk, m4a4 evil daimyo, m4a1s cyrex, m4a1s hyperbeast, deagle conspiracy, p250 supernova, 
 #glock water elemental, stat trak glock grinder, p90 elite build, mp9 ruby poison dart, mp9 deadly poison, usps stainless
@@ -31,4 +37,7 @@ with open(home_dir + "/cs_item_list.txt", 'r') as file:
 #%% PULL DATA
 
 for item in cs_item_list:
-    df = pull_cs_price_history(item,request,data_dir)
+    df = pull_cs_price_history(item,request,cs_data_dir)
+    
+for item in dota_item_list:
+    df = pull_dota_price_history(item,request,dota_data_dir)
--- code/pull_mkt_data_func.py
+++ code/pull_mkt_data_func.py
@@ -5,6 +5,8 @@ import numpy as np
 import pandas as pd
 from steamcrawl import Request
 import re
+from datetime import datetime
+#from dateutil.parser import parse
 
 #%% PULL COUNTER STRIKE PRICE DATA
 def pull_cs_price_history(item,request,out_dir):
@@ -17,32 +19,76 @@ def pull_cs_price_history(item,request,out_dir):
 
     Returns:
         pull_df(DataFrame): Pandas dataframe with columns: 
-                            date (str):            date of data point
-                            median_price (float):  median price of data point, rounded to 3 decimals
-                            volume_sold (int):     volume sold of data point at date and median_price
-                            item (str):            item name without StatTrak and wear
-                            weapon (str):          weapon of item
-                            skin (str):            item skin, ie without StatTrak, weapon name, and wear
-                            wear (str):            item wear rating
-                            stattrak (int):        {0,1} dummy for StatTrak
+                            date (str):                     date of data point
+                            py_date (datetime.datetime):    datetime object of date
+                            median_price (float):           median price of data point, rounded to 3 decimals
+                            volume_sold (int):              volume sold of data point at date and median_price
+                            item (str):                     item name without StatTrak and wear
+                            weapon (str):                   weapon of item
+                            skin (str):                     item skin, ie without StatTrak, weapon name, and wear
+                            wear (str):                     item wear rating
+                            stattrak (int):                 {0,1} dummy for StatTrak
+                            treated_unit (int):             {0,1} dummy, dota2 = 0, cs2 = 1
+                            post_treat (int):               {0,1} dummy, equals 1 if py_date >= March 29, 2018
         {item}_price_history.csv (csv): csv output of pull_df
     """
     
     pull_df = request.get_price_history(item_name=item,appid="730")
+    #create python datetime format for date
+    pull_df['py_date'] = pd.to_datetime(pull_df['date'], format='%b %d %Y')
     #use item name to get item qualities
     pull_df['item'] = item.replace("StatTrak™ ","").replace(" (","").replace(re.search(r'\((.*?)\)',item).group(1),"").replace(")","") #item name excluding StatTrak and wear
     pull_df['weapon'] = item.split(" |")[0].replace("StatTrak™ ","") #weapon
     pull_df['skin'] = pull_df['item'][0].split(" | ")[1] #weapon skin, eg "Redline"
     pull_df['wear'] = re.search(r'\((.*?)\)',item).group(1) #item wear rating
+    pull_df['treated_unit'] = 1
     #assign StatTrak value
     if "StatTrak™" in item:
         pull_df['stattrak'] = 1
     else:
         pull_df['stattrak'] = 0
+    #assign pre/post-intervention status (Mar 29 2018)
+    pull_df['post_treat'] = 0
+    pull_df.loc[pull_df['py_date'] >= datetime(2018,3,29), 'post_treat'] = 1
     #clean a little
     pull_df['median_price'] = pull_df["median_price"].round(3)
+    pull_df = pull_df[['date','py_date','median_price','volume_sold','item','weapon','skin','wear','stattrak','treated_unit','post_treat']]
     #export
     pull_df.to_csv(f'{out_dir}/{item.replace("™","").replace("|","").replace(" ","")}_price_history.csv',index=False)
     return pull_df
 
 #%% PULL DOTA 2 PRICE DATA
+def pull_dota_price_history(item,request,out_dir):
+    """Wrapper for steamcrawl's get_price_history method with settings for DOTA 2 pre-applied.
+
+    Args:
+        request (steamcrawl.Request):   steamcrawl.Request object containing a 'steamLoginSecure' key which must be updated daily
+        item (str):                     Counter Strike Community Market full item name
+        out_dir (str):                  directory to output .csv
+
+    Returns:
+        pull_df(DataFrame): Pandas dataframe with columns: 
+                            date (str):                     date of data point
+                            py_date (datetime.datetime):    datetime object of date
+                            median_price (float):           median price of data point, rounded to 3 decimals
+                            volume_sold (int):              volume sold of data point at date and median_price
+                            item (str):                     item name
+                            treated_unit (int):             {0,1} dummy, dota2 = 0, cs2 = 1
+                            post_treat (int):               {0,1} dummy, equals 1 if py_date >= March 29, 2018
+        {item}_price_history.csv (csv): csv output of pull_df
+    """
+    pull_df = request.get_price_history(item_name=item,appid="570")
+    #create python datetime format for date
+    pull_df['py_date'] = pd.to_datetime(pull_df['date'], format='%b %d %Y')
+    #use item name to get item qualities
+    pull_df['item'] = item #item name
+    pull_df['treated_unit'] = 0
+    #assign pre/post-intervention status (Mar 29 2018)
+    pull_df['post_treat'] = 0
+    pull_df.loc[pull_df['py_date'] >= datetime(2018,3,29), 'post_treat'] = 1 
+    #clean a little
+    pull_df['median_price'] = pull_df["median_price"].round(3)
+    pull_df = pull_df[['date','py_date','median_price','volume_sold','item','treated_unit','post_treat']]
+    #export
+    pull_df.to_csv(f'{out_dir}/{item.replace("™","").replace("|","").replace(" ","")}_price_history.csv',index=False)
+    return pull_df
