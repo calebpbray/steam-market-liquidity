0e7f184571e1d8229c8e1cf4c2d4cd4aeb938447
Caleb Bray <calebpbray@gmail.com>
2025-06-26 10:35:08 -0500

    Added additional CS2 skins, need to add more dota2 skins, also maybe need to check and see if we need to try and get things in similar collections for fixed effects.

--- code/plot_mkt_data.R
+++ code/plot_mkt_data.R
@@ -11,17 +11,17 @@ ggplot(NULL, aes(x=r_date, y=log(median_price))) +
   geom_point(data=df_cs, color="#EDA338") + 
   geom_point(data=df_dota, color= "#FF0000") + 
   geom_vline(xintercept = as.Date("2018-03-29")) + 
-  geom_smooth(data=subset(df_cs, r_date >= as.Date("2016-03-29") & r_date <= as.Date("2018-03-29")), method='lm') +
-  geom_smooth(data=subset(df_dota, r_date >= as.Date("2016-03-29") & r_date <= as.Date("2018-03-29")), method='lm') +
+  geom_smooth(data=subset(df_cs, r_date >= as.Date("2016-03-29") & r_date <= as.Date("2018-03-29")), method='lm', color="blue") +
+  geom_smooth(data=subset(df_dota, r_date >= as.Date("2016-03-29") & r_date <= as.Date("2018-03-29")), method='lm', color="navyblue") +
   geom_smooth(data=subset(df_cs, r_date >= as.Date("2018-03-29") & r_date <= as.Date("2020-03-29")), method='lm', color="green") +
-  geom_smooth(data=subset(df_dota, r_date >= as.Date("2018-03-29") & r_date <= as.Date("2020-03-29")), method='lm', color="green")
+  geom_smooth(data=subset(df_dota, r_date >= as.Date("2018-03-29") & r_date <= as.Date("2020-03-29")), method='lm', color="green4")
 
 ggplot(NULL, aes(x=r_date, y=log(volume_sold))) + 
   geom_point(data=df_cs, color="#EDA338") + 
   geom_point(data=df_dota, color= "#FF0000") + 
   geom_vline(xintercept = as.Date("2018-03-29")) + 
-  geom_smooth(data=subset(df_cs, r_date >= as.Date("2016-03-29") & r_date <= as.Date("2018-03-29")), method='lm') +
-  geom_smooth(data=subset(df_dota, r_date >= as.Date("2016-03-29") & r_date <= as.Date("2018-03-29")), method='lm') +
+  geom_smooth(data=subset(df_cs, r_date >= as.Date("2016-03-29") & r_date <= as.Date("2018-03-29")), method='lm', color="blue") +
+  geom_smooth(data=subset(df_dota, r_date >= as.Date("2016-03-29") & r_date <= as.Date("2018-03-29")), method='lm', color="navyblue") +
   geom_smooth(data=subset(df_cs, r_date >= as.Date("2018-03-29") & r_date <= as.Date("2020-03-29")), method='lm', color="green") +
-  geom_smooth(data=subset(df_dota, r_date >= as.Date("2018-03-29") & r_date <= as.Date("2020-03-29")), method='lm', color="green")
+  geom_smooth(data=subset(df_dota, r_date >= as.Date("2018-03-29") & r_date <= as.Date("2020-03-29")), method='lm', color="green4")
 
